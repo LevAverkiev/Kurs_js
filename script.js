408464154ab0d7e429c49a12fa122bcb912aa6cd
LevAverkiev <levaverkiev@gmail.com>
2022-01-30 22:58:01 +0300

    Сделал все задания

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ let rollback;
 let fullPrice;
 let adaptive;
 
-alert("Это я!");
+title = 'My Project';
+screens = 'Простые, Сложные, Интерактивные';
+screenPrice = 95;
+rollback = 51;
+fullPrice = 100000000;
+adaptive = true;
 
-console.log("Я всё ещё тут!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)));
