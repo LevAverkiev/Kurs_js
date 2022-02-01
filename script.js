775464811abcdb19a62148e0357cc90cac42b905
LevAverkiev <levaverkiev@gmail.com>
2022-02-01 09:32:14 +0300

    Все задания готовы

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-let num = 266219;
-let arr = String(num).split("");
-let multiplication = 1;
-let degree;
-let numTwo;
+let title = 'My Project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 95;
+let rollback = 51;
+let fullPrice = 100000000;
+let adaptive = true;
 
-for (let i = 0; i < arr.length; i++) {
-    multiplication *= Number(arr[i]);
+title = prompt('Как называется наш проект?');
+console.log('Мой проект называется: ' + title);
+
+screens = prompt('Какие типы экранов нужно разработать?(простые, сложные, интерактивные)');
+console.log('Нужно разработать ' + screens + ' типы экранов');
+
+screenPrice = +prompt('Сколько будет стоить данная работа?');
+console.log('Данная работа будет стоит ' + screenPrice + ' рублей');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
+console.log('Дополнительный тип услуги: ' + service1 + '. Это будет стоить: ' + servicePrice1 +
+    '. Дополнительный тип услуги: ' + service2 + '. Это будет стоить ' + servicePrice2 + '.');
+
+fullPrice = parseFloat(screenPrice) + parseFloat(servicePrice1) + parseFloat(servicePrice2);
+console.log('Итоговая стоимость работы: ' + fullPrice);
+
+let servicePercentPrice = Math.ceil(fullPrice - 100);
+console.log('Итоговая стоимость проекта: ' + servicePercentPrice);
+
+
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice > 0 && fullPrice < 15000) {
+    console.log('Скидка не предусмотрена');
+} else {
+    console.log('Что-то пошло не так');
 }
-console.log(multiplication);
-
-degree = multiplication ** 3;
-console.log(degree);
-
-numTwo = Number(String(degree).substr(0, 2));
-console.log(numTwo);
