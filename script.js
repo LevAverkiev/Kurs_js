991961b9b96c825053497c0819f2e310d2576f05
LevAverkiev <levaverkiev@gmail.com>
2022-02-08 23:11:05 +0300

    Сделал все задания

--- script.js
+++ script.js
@@ -1,97 +1,33 @@
-let title
-let screens
-let screenPrice
-let adaptive
-let rollback = 10
-let allServicePrices
-let fullPrice
-let servicePersentPrice
-let service1
-let service2
+// "Загадывание случайного числа от 1 до 100"
 
-const isNumber = function (num) {
+let numRandom = Math.ceil(Math.random() * 100)
+
+function isNum(num) {
     return !isNaN(parseFloat(num)) && isFinite(num)
 }
 
-const asking = function () {
-    title = prompt('Как называется ваш проект?', 'Калькулятор верстки')
-    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, сложные')
+function userGuessNumber() {
+    let numUser = prompt('Угадай число от 1 до 100')
 
-    do {
-        screenPrice = prompt('Сколько будет стоить данная работа?')
-    } while (!isNumber(screenPrice))
+    if (numUser === null) {
+        return alert('Игра окончена')
+    }
 
-    adaptive = confirm('Нужен ли адаптив на сайте?')
-}
+    if (isNum(numUser)) {
+        numUser = +numUser
 
-const getAllServicePrices = function () {
-    let sum = 0
-
-    for (let i = 0; i < 2; i++) {
-        let price = 0
-
-        if (i === 0) {
-            service1 = prompt('Какой дополнительный тип услуги нужен?')
-        } else if (i === 1) {
-            service2 = prompt('Какой дополнительный тип услуги нужен?')
+        if (numUser > numRandom) {
+            alert('Загаданное число меньше')
+        } else if (numUser < numRandom) {
+            alert('Загаданное число больше')
+        } else if (numUser === numRandom) {
+            alert('Поздравляю, Вы угадали!!!')
+            return numUser;
         }
-
-        do {
-            price = prompt('Сколько это будет стоить?')
-        } while (!isNumber(price))
-
-        sum += +price
-    }
-
-    return sum
-}
-
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-}
-
-const getFullPrice = function () {
-    return +screenPrice + allServicePrices
-}
-
-const getServicePercentPrice = function () {
-    return fullPrice - (fullPrice * (rollback / 100))
-}
-
-const getTitle = function () {
-    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
-}
-
-const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return 'Даем скидку в 10%'
-    } else if (price >= 15000 && price < 30000) {
-        return 'Даем скидку в 5%'
-    } else if (price >= 0 && price < 15000) {
-        return 'Скидка не предусмотрена'
     } else {
-        return 'Что-то пошло не так'
+        alert('Введи число!')
     }
+    return userGuessNumber()
 }
 
-asking()
-allServicePrices = getAllServicePrices()
-fullPrice = getFullPrice()
-servicePersentPrice = getServicePercentPrice()
-title = getTitle()
-
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
-
-console.log('allServicePrices', allServicePrices);
-
-console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
-
-console.log(screens.length);
-console.log(servicePersentPrice);
-
-console.log('Стоимость верстки экранов ' + screenPrice + ' юани" и "Стоимость разработки сайта ' + fullPrice + " юани");
+console.log(userGuessNumber())
