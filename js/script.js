41641ba20bc5340d9291fdf0963fa6a4b8c5864a
LevAverkiev <levaverkiev@gmail.com>
2022-02-16 21:13:28 +0300

    Исправил по комменту

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 'use strict';
-const title = document.getElementsByTagName('h1'),
-    handlerBtn = document.getElementsByClassName('.handler_btn'),
+const title = document.getElementsByTagName('h1')[0],
+    startBtn = document.getElementsByClassName('.handler_btn')[0],
+    resetBtn = document.getElementsByClassName('.handler_btn')[1],
     screenBtn = document.querySelector('.screen-btn'),
     otherItemsPercent = document.querySelectorAll('.other-items.percent'),
     otherItemsNumber = document.querySelectorAll('.other-items.number '),
     typeRange = document.querySelector('.rollback  input[type="range"]'),
     spanRangeValue = document.querySelector('.rollback  span'),
-    totalInput = document.getElementsByClassName('total-input');
+    totalInput = document.getElementsByClassName('total-input'),
+    total = document.getElementsByClassName("total-input")[0],
+    totalCount = document.getElementsByClassName("total-input")[1],
+    totalCountOther = document.getElementsByClassName("total-input")[2],
+    totalFullCount = document.getElementsByClassName("total-input")[3],
+    totalCountRollback = document.getElementsByClassName("total-input")[4];
 
 let screen = document.querySelectorAll('.screen');
 
